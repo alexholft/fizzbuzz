90d5751945da93448df48b9c1fcf95969a806a48
alexholft <58967465+alexholft@users.noreply.github.com>
2021-08-23 16:33:59 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -19,3 +19,31 @@ print "Plain Text : " + text
 print "Shift pattern : " + str(s)
 print "Cipher: " + encrypt(text,s)
 
+#Caesar Cipher
+
+## Overview of Caesar Cipher
+
+ #- A cipher that is believed to be invented by the Roman General, Caesar.
+ #- It is known to be the simplest and well-known Cipher/Decoding method.
+
+ #- Input: A string of lowercase or uppercase letters((a to z, or A to Z), in alphabetical order)
+ #- An integer between 0-25, indicating the shift of number
+ #- There are 26 characters in English alphabet. And index starts from 0.
+ #- So 0 to 25 basically indicates 26 English characters in alphabetical order at the input stage.)
+
+### Procedure
+
+ #- Traverse the given text, one English character at a time.
+ #- And let's suppose that the shift is 3, meaning that in an alphabetical order,
+ #- shift the string of all English Characters to the right 3(Shift) times, one by one.
+ #- As a result, general alphabetical order changes, from
+ #- A-B-C-D-E... -W-X-Y-Z to (Cipher code)
+ #- X-Y-Z-A-B... -T-U-V-W. (Decipher code)
+ #- So, depending on how big the shift is, the new string is generated.
+
+#### Output
+
+ #- For example,
+ #- Text : ZXWW
+ #- Shift : 3
+ #- Cipher : WATT
